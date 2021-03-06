8add5424bba3f0afb8072e3a2b4f5c503ad6fd5e
Yasen Kostov <qskatak04@gmail.com>
2021-03-06 11:55:41 +0200

    Install arc_ecto library

--- mix.exs
+++ mix.exs
@@ -51,7 +51,8 @@ defmodule BgsiteOfficial.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:arc_ecto, "~> 0.11.0"}
     ]
   end
 
