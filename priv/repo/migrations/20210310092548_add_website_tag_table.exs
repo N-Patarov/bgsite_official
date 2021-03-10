32c13d4333ee57d686cc53250b872c64a51a852d
Yasen Kostov <qskatak04@gmail.com>
2021-03-10 11:51:06 +0200

    Create migration

---
+++ priv/repo/migrations/20210310092548_add_website_tag_table.exs
@@ -0,0 +1,15 @@
+defmodule BgsiteOfficial.Repo.Migrations.AddWebsiteTagTable do
+  use Ecto.Migration
+
+  def change do
+
+    create table(:website_tag, primary_key: false) do
+      add(:website_id, references(:websites, on_delete: :delete_all), primary_key: true)
+      add(:tag_id, references(:tags, on_delete: :delete_all), primary_key: true)
+      timestamps()
+    end
+    create(index(:website_tag, [:website_id]))
+    create(index(:website_tag, [:tag_id]))
+
+  end
+end
