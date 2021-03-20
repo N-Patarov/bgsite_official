7a3a30778410541f4c2cc062260cdd90a9541936
N-Patarov <nikolayp1707@gmail.com>
2021-03-20 22:58:03 +0200

    fix-default2

--- lib/bgsite_official_web/router.ex
+++ lib/bgsite_official_web/router.ex
@@ -19,7 +19,7 @@ defmodule BgsiteOfficialWeb.Router do
 
   scope "/", BgsiteOfficialWeb do
     pipe_through :browser
-    get "/", WebsitesControlller, :index
+    get "/", WebsitesController, :index
   end
 
   # Other scopes may use custom stacks.
