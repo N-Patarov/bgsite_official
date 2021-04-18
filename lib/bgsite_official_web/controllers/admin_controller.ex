42face8593fb0147613ddea68e2c4881e25e71c8
AlenBG <alen.pechovski@abv.bg>
2021-04-18 12:52:04 +0300

    adminPage

--- lib/bgsite_official_web/controllers/admin_controller.ex
+++ lib/bgsite_official_web/controllers/admin_controller.ex
@@ -2,9 +2,15 @@ defmodule  BgsiteOfficialWeb.AdminController do
   use BgsiteOfficialWeb, :controller
   alias BgsiteOfficial.Requests
   alias BgsiteOfficial.Requests.Request
-  
+  alias BgsiteOfficial.Contacts
+  alias BgsiteOfficial.Contacts.Feedback
+  alias BgsiteOfficial.Categories
+  alias BgsiteOfficial.Categories.Tag
+
   def index(conn, _params) do
     requests = Requests.list_requests()
-    render(conn, "index.html", requests: requests)
+    feedback = Contacts.list_feedback()
+    tags = Categories.list_tags()
+    render(conn, "index.html", requests: requests, feedback: feedback, tags: tags)
   end
 end
