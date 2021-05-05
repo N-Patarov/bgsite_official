6d0e30975878b82afce213cb8b7e45d4dbe4ca3a
N-Patarov <nikolayp1707@gmail.com>
2021-05-05 18:07:14 +0300

    fix

--- lib/bgsite_official_web/controllers/index_controller.ex
+++ lib/bgsite_official_web/controllers/index_controller.ex
@@ -1,8 +1,8 @@
 defmodule  BgsiteOfficialWeb.IndexController do
   use BgsiteOfficialWeb, :controller
 
-  alias BgsiteOfficial.Websites
-
+  alias BgsiteOfficial.Home
+  alias BgsiteOfficial.Home.Websites
   def index(conn, _params) do
     websites = Home.list_websites(_params)
     render(conn, "index.html", websites: websites)
