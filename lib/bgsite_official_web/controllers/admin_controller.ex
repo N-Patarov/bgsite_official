43e30ad646f0b9722896119945df8ac817f48ab8
Yasen Kostov <qskatak04@gmail.com>
2021-05-12 17:57:06 +0300

    Add admin/websites

--- lib/bgsite_official_web/controllers/admin_controller.ex
+++ lib/bgsite_official_web/controllers/admin_controller.ex
@@ -6,11 +6,18 @@ defmodule  BgsiteOfficialWeb.AdminController do
   alias BgsiteOfficial.Contacts.Feedback
   alias BgsiteOfficial.Categories
   alias BgsiteOfficial.Categories.Tag
+  alias BgsiteOfficial.Home
+  alias BgsiteOfficial.Home.Websites
 
   def index(conn, _params) do
     requests = Requests.list_requests()
     feedback = Contacts.list_feedback()
     tags = Categories.list_tags()
     render(conn, "index.html", requests: requests, feedback: feedback, tags: tags)
+  end
+
+  def websites(conn, params) do
+    websites = Home.list_websites(params)
+    render(conn, "websites.html", websites: websites)
   end
 end
