801a3168e47a96a46e73a195c749af331b8cbd7c
N-Patarov <nikolayp1707@gmail.com>
2021-06-04 19:02:34 +0300

    fix-some-bugs

--- lib/bgsite_official_web/live/tag_live.ex
+++ lib/bgsite_official_web/live/tag_live.ex
@@ -9,13 +9,14 @@ defmodule BgsiteOfficialWeb.TagLive do
 
   def render(assigns) do
     render TagView, "show.html", assigns
-   end
 
-  @impl true
+   end
+     @impl true
   def mount(params, %{"admin_token" => admin_token} = session, socket) do
     tag = Categories.get_tag!(params["id"])
     websites = tag.websites |> Repo.preload(:tags)
     admin = Accounts.get_admin_by_session_token(admin_token)
+    socket = assign(socket,:likes,0)
     socket = assign(
         socket,
         tag: tag,
@@ -23,6 +24,13 @@ defmodule BgsiteOfficialWeb.TagLive do
         current_admin: admin
       )
     {:ok, socket}
+  end
+
+  def handle_event("like",_,socket)do
+    socket=update(socket,:likes, &(&1+1))
+    {:noreply,socket}
+
+
   end
 
 end
