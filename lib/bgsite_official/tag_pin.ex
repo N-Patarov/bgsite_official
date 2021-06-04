f7d8b06a3c2dc47508c80fd6015b5c0ba983af22
AlenBG <alen.pechovski@abv.bg>
2021-06-04 20:37:17 +0300

    tag_pin2

---
+++ lib/bgsite_official/tag_pin.ex
@@ -0,0 +1,13 @@
+defmodule BgsiteOfficial.TagPin do
+  @moduledoc """
+  The TagPin context.
+  """
+  import Ecto.Query, warn: false
+  alias BgsiteOfficial.Repo
+  alias BgsiteOfficial.Categories.Tags
+  alias BgsiteOfficial.TagPin
+  alias BgsiteOfficial.Categories.TagPin
+  @doc """
+  Returns the list of tag pins.
+  """
+end
