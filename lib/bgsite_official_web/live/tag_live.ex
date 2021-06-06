32b7b41906094b0795708fb5291e8412a533feac
N-Patarov <nikolayp1707@gmail.com>
2021-06-06 10:07:16 +0300

    phx-value

--- lib/bgsite_official_web/live/tag_live.ex
+++ lib/bgsite_official_web/live/tag_live.ex
@@ -16,9 +16,9 @@ defmodule BgsiteOfficialWeb.TagLive do
     tag = Categories.get_tag!(params["id"])
     websites = tag.websites |> Repo.preload(:tags)
     admin = Accounts.get_admin_by_session_token(admin_token)
-    socket = assign(socket,:likes,0)
     socket = assign(
         socket,
+        likes: 0,
         tag: tag,
         websites_for_tag: websites,
         current_admin: admin
@@ -26,9 +26,10 @@ defmodule BgsiteOfficialWeb.TagLive do
     {:ok, socket}
   end
 
-  def handle_event("add_like", %{"website-id" => website_id}, socket) do
+  def handle_event("add_like", %{"website-id" => website_id,"likevalue" => 1}, socket) do
     Categories.add_website_like(website_id)
     {:noreply, socket}
   end
+
 
 end
