99f48e71a54146184a442acf5f54084c6bc7ab77
Yasen Kostov <qskatak04@gmail.com>
2021-06-21 14:10:13 +0300

    Setting Up an Ecto Repository

--- config/config.exs
+++ config/config.exs
@@ -34,4 +34,9 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+
+
+config :bgsite_official,
+  ecto_repos: [BgsiteOfficial.Repo]
+
 import_config "#{Mix.env()}.exs"
