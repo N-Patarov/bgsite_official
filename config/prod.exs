ceaedcdfc3b04717ef708c0a2fa7929a42709861
Yasen Kostov <qskatak04@gmail.com>
2021-07-12 17:39:47 +0300

    docker things

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :bgsite_official, BgsiteOfficial.Endpoint,
-  url: [host: "example.com", port: 4000],
+  url: [host: "izberi.site", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"# Do not print debug messages in production
 config :logger, level: :info# ## Using releases (Elixir v1.9+)
 #
