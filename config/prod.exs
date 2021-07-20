f08917fc970a4d8a1df1e9a8adafb53e6addeed1
Yasen Kostov <qskatak04@gmail.com>
2021-07-20 15:04:52 +0000

    host setup

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :bgsite_official, BgsiteOfficialWeb.Endpoint,
-  url: [host: "localhost", port: 80],
+  url: [host: "izberi.site", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
