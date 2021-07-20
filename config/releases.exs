b424c6683891f56872c8baa0fd39b7134e70bfdb
Yasen Kostov <qskatak04@gmail.com>
2021-07-20 13:53:16 +0300

    add phx_releases config

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -35,7 +35,7 @@ config :bgsite_official, BgsiteOfficialWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :bgsite_official, BgsiteOfficialWeb.Endpoint, server: true
+config :bgsite_official, BgsiteOfficialWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
